57c8d4b301057fa94cfb3555d1c02427f85caa98
oscar <15737128231@163.com>
2016-08-29 14:50:10 +0800

    版本更新对话框及进入主界面

--- app/src/main/java/com/oscar/mobilesafe/activity/MainActivity.java
+++ app/src/main/java/com/oscar/mobilesafe/activity/MainActivity.java
@@ -1,8 +1,14 @@
 package com.oscar.mobilesafe.activity;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.TextView;
@@ -20,7 +26,16 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int UPDATE_VERSION = 100;
+    private static final int ENTER_HOME = 101;
+    private static final int URL_ERROR = 102;
+    private static final int IO_ERROR = 103;
+    private static final int JSON_ERROR = 104;
+
+
     private TextView mTvVersion;
+
+    private Context mContext;
 
     private int mLocalVersionCode;
     private static String TAG = "MainActivity";
@@ -28,6 +43,74 @@ public class MainActivity extends AppCompatActivity {
     private String mVersionCode;//版本号
     private String mVersionDes;//版本描述
     private String mDownloadUrl;//更新版本下载地址
+
+
+
+    private Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case UPDATE_VERSION:
+                    showUpdateDialog();
+                    break;
+                case ENTER_HOME:
+                    enterHome();
+                    break;
+                case URL_ERROR:
+                    break;
+                case IO_ERROR:
+                    break;
+                case JSON_ERROR:
+                    break;
+            }
+        }
+    };
+
+    /**
+     * 进入主界面
+     */
+    private void enterHome() {
+        Intent intent = new Intent(mContext, HomeActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    /**
+     * 提示版本更新对话框
+     */
+    private void showUpdateDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
+        builder.setIcon(R.mipmap.ic_launcher);
+        builder.setTitle("版本更新");
+        builder.setMessage(mVersionDes);
+        builder.setPositiveButton("立即更新", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                downloadApk();
+            }
+        });
+
+        builder.setNegativeButton("稍后再说", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                enterHome();
+            }
+        });
+
+        builder.setOnCancelListener(new DialogInterface.OnCancelListener() {
+            @Override
+            public void onCancel(DialogInterface dialog) {
+
+            }
+        });
+    }
+
+    /**
+     * 下载apk
+     */
+    private void downloadApk() {
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,7 +123,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initDatas() {
-        mTvVersion.setText("版本号：" + getVersionName());
+        mContext = this;
+
+        mTvVersion.setText("版本名称：" + getVersionName());
 
         mLocalVersionCode = getLocalVersionCode();
 
@@ -49,14 +134,17 @@ public class MainActivity extends AppCompatActivity {
 
     private void checkVersion() {
         final String urlStr = "http://192.168.0.10:8080/version.json";
+        final Message msg = Message.obtain();
         new Thread(){
             @Override
             public void run() {
+                //开始时间
+                long startTime = System.currentTimeMillis();
                 try {
                     URL url = new URL(urlStr);
                     HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-                    conn.setConnectTimeout(1000 * 10);
-                    conn.setReadTimeout(1000 * 10);
+                    conn.setConnectTimeout(2000);
+                    conn.setReadTimeout(2000);
                     conn.setRequestMethod("GET");
                     if(conn.getResponseCode() == 200) {
                         InputStream is = conn.getInputStream();
@@ -72,13 +160,33 @@ public class MainActivity extends AppCompatActivity {
                         Log.i(TAG, mVersionCode);
                         Log.i(TAG, mVersionDes);
                         Log.i(TAG, mDownloadUrl);
+
+                        if(mLocalVersionCode < Integer.parseInt(mVersionCode)) {
+                            msg.what = UPDATE_VERSION;
+                        } else {
+                            msg.what = ENTER_HOME;
+                        }
                     }
                 } catch (MalformedURLException e) {
                     e.printStackTrace();
+                    msg.what = URL_ERROR;
                 } catch (IOException e) {
                     e.printStackTrace();
+                    msg.what = IO_ERROR;
                 } catch (JSONException e) {
                     e.printStackTrace();
+                    msg.what = JSON_ERROR;
+                } finally {
+                    //结束时间
+                    long endTime = System.currentTimeMillis();
+                    if(endTime - startTime < 4000) {
+                        try {
+                            Thread.sleep(4000 - (endTime - startTime));
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    mHandler.sendMessage(msg);
                 }
 
             }
